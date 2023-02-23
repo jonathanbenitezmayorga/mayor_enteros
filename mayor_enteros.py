e885fd2e169c2f2f588c7d83078fe9969549d584
jonathanbenitezmayorga <jonathanbenitez@colegioguanenta.edu.co>
2023-02-23 16:09:42 -0500

    Se creo el digrama de flujo y se agrego al README

---
+++ mayor_enteros.py
@@ -0,0 +1,16 @@
+# input
+
+X = int(input("ingrese un numero de X: "))
+
+Y = int(input("ingrese el numero de Y: "))
+
+# processing
+if X == Y:
+    # output
+    print("los numeros son iguales...")
+else:
+    if X > Y:
+        mayor = X
+    else:
+            mayor = Y
+    print("el mayor entre: " + str(X) + " y " + str(Y) + " es " + str(mayor))
